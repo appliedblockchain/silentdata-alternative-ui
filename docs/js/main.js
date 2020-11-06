377fbdcc27f42e082bf20ada432cef483b0c962c
Mario Gemoll <mario@appliedblockchain.com>
2020-11-06 20:42:20 +0000

    Add basic skeleton to load and parse a proof

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,36 @@
-import sleep from './sleep.js';
-async function run() {
-    await sleep(1000);
-    alert('hello world!');
+const proofDataTextarea = document.querySelector('textarea#proof_data');
+const loadProofButton = document.querySelector('button#load_proof');
+const readableDataTable = document.querySelector('table#readable_data');
+function td(content) {
+    const td = document.createElement('td');
+    td.innerText = content;
+    return td;
 }
-run();
+function tr(tds) {
+    const tr = document.createElement('tr');
+    for (const td of tds) {
+        tr.appendChild(td);
+    }
+    return tr;
+}
+function handleProofDataUpdate() {
+    let proof;
+    try {
+        proof = JSON.parse(proofDataTextarea.value);
+    }
+    catch (e) {
+        alert('Cannot parse proof data');
+    }
+    readableDataTable.append(tr([
+        td('Minimum balance'),
+        td(proof.configuration.minBalance.toString())
+    ]));
+}
+async function init() {
+    const res = await fetch('proof.json');
+    proofDataTextarea.value = await res.text();
+    handleProofDataUpdate();
+}
+loadProofButton.addEventListener('click', handleProofDataUpdate);
+init();
+export {};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,48 @@
 import sleep from './sleep.js'
-async function run() {
-	await sleep(1000)
-	alert('hello world!')
+
+const proofDataTextarea: HTMLTextAreaElement = document.querySelector('textarea#proof_data')
+const loadProofButton: HTMLButtonElement = document.querySelector('button#load_proof')
+const readableDataTable: HTMLTableElement = document.querySelector('table#readable_data')
+
+type Proof = {
+  configuration: {
+    minBalance: number
+  }
 }
-run()
+
+function td(content: string): HTMLTableCellElement {
+  const td = document.createElement('td')
+  td.innerText = content
+  return td
+}
+
+function tr(tds: HTMLTableCellElement[]): HTMLTableRowElement {
+  const tr = document.createElement('tr')
+  for (const td of tds) {
+    tr.appendChild(td)
+  }
+  return tr
+}
+
+function handleProofDataUpdate() {
+  let proof: Proof
+  try {
+    proof = JSON.parse(proofDataTextarea.value)
+  } catch (e) {
+    alert('Cannot parse proof data')
+  }
+  readableDataTable.append(tr([
+    td('Minimum balance'),
+    td(proof.configuration.minBalance.toString())
+  ]))
+}
+
+async function init() {
+  const res = await fetch('proof.json')
+  proofDataTextarea.value = await res.text()
+  handleProofDataUpdate()
+}
+
+loadProofButton.addEventListener('click', handleProofDataUpdate)
+
+init()
